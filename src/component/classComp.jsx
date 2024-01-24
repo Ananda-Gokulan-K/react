92bf7c98156434361ab894ea27b194ed3741f0de
Ananda-Gokulan-K <anandagokulan3@gmail.com>
2024-01-24 16:25:08 +0530

    o

---
+++ src/component/classComp.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Testcomp from "./testComponent";
+class Reiteratorclasscomp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Testcomp/>
+        <p>Bla bla bla</p>
+      </div>
+    )
+  }
+}
+
+export default Reiteratorclasscomp;
+
+//reiteratorclasscomp - comp-name
+//
---
+++ src/component/testComp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Reiteratorclasscomp from "./classComp";
+ function Testcomp() {
+  return (
+    <div>
+        <Reiteratorclasscomp/>
+      <p>Bla bla bla</p>
+    </div>
+  );
+};
+export default Testcomp;
+
